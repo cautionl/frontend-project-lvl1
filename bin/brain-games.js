4b345f400c2f9f7906c7064d17cb03f0ccdb9a85
Vladimir Basalaev <cautionl@mail.ru>
2020-07-29 23:10:02 +0300

    Fix brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-// eslint-disable-next-line import/no-absolute-path
-import { greeting } from '/home/vladimir/Documents/Project/frontend-project-lvl1/src/cli.js';
+// eslint-disable-next-line import/no-absolute-path,import/no-unresolved
+import { greeting } from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 greeting();
