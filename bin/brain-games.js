e16fcd3494949681e77225bdd9ad90e4e1416bfe
Vladimir Basalaev <cautionl@mail.ru>
2020-07-29 22:46:58 +0300

    Delete .eslintrc.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+// eslint-disable-next-line import/no-absolute-path
 import { greeting } from '/home/vladimir/Documents/Project/frontend-project-lvl1/src/cli.js';
+
 console.log('Welcome to the Brain Games!');
 greeting();
