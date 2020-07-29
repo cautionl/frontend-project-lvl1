c40d66fca4a31d2351e241df4485f8bb67f5debd
Vladimir Basalaev <cautionl@mail.ru>
2020-07-29 19:26:13 +0300

    Add Makefile

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
- export const greeting = () => {
-     const userName = readlineSync.question('May I have your name? ');
-     console.log(`Hi ${userName}!`);
- };
+// eslint-disable-next-line import/prefer-default-export
+export const greeting = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi ${userName}!`);
+};
